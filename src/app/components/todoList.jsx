8716ccd39a473a79da63cfd95ace7552d37effcd
XueQian <qian.xue@rea-group.com>
2017-02-25 18:10:24 +0800

    can show todos

--- src/app/components/todoList.jsx
+++ src/app/components/todoList.jsx
@@ -1,13 +1,38 @@
 const React = require('react');
+const axios = require('axios');
 
 let TodoList = React.createClass({
+    getInitialState: function () {
+
+        return {
+            data: {items: []}
+        };
+    },
+    componentDidMount: function () {
+
+        const that = this;
+
+        axios.get("http://localhost:4010/todoitems")
+            .then(function (response) {
+                that.setState({data: response.data});
+            });
+    },
 
     render: function () {
+        const items = this.state.data.items;
         return (
             <div className="container">
                 <div className="homepage">
                     <h1>todos</h1>
                     <input></input>
+                    <ul>
+                        {items.map(function (item) {
+                            return <li>
+                                <input type="checkbox" value={item.id}/>{item.text}
+                            </li>
+                        }) }
+                    </ul>
+
                 </div>
             </div>
         );
