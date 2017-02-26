9ef16416c242a921165f418e575c1ccbc7044c09
XueQian <qian.xue@rea-group.com>
2017-02-26 11:05:02 +0800

    can add new todo

--- src/app/components/todoList.jsx
+++ src/app/components/todoList.jsx
@@ -1,11 +1,14 @@
 const React = require('react');
 const axios = require('axios');
+const ENTER_KEY = 13;
 
 let TodoList = React.createClass({
+
     getInitialState: function () {
 
         return {
-            data: {items: []}
+            items: [],
+            newItem: ''
         };
     },
     componentDidMount: function () {
@@ -14,17 +17,57 @@ let TodoList = React.createClass({
 
         axios.get("http://localhost:4010/todoitems")
             .then(function (response) {
-                that.setState({data: response.data});
+                that.setState({items: response.data.items});
             });
     },
 
+    submitNewItem: function (event) {
+
+        if (event.keyCode !== ENTER_KEY) {
+            return;
+        }
+        event.preventDefault();
+
+        let newInput = this.state.newItem.trim();
+
+        if (newInput) {
+            let newItem = {
+                "done": false,
+                "id": this.state.items.length + 1,
+                "text": newInput,
+                "timestamp": new Date()
+            };
+
+            this.state.items.push(newItem);
+
+            axios.post('http://localhost:4010/todoitems', newItem)
+                .then(function (response) {
+                    console.log(response.status + "post a new item");
+                });
+
+            this.setState({newItem: ''});
+        }
+    },
+
+    createNewItem: function (event) {
+        this.setState({newItem: event.target.value});
+    },
+
     render: function () {
-        const items = this.state.data.items;
+
+        const items = this.state.items;
         return (
             <div className="container">
                 <div className="homepage">
+
                     <h1>todos</h1>
-                    <input></input>
+
+                    <input
+                        placeholder="What need to be done?"
+                        value={this.state.newItem}
+                        onKeyDown={this.submitNewItem}
+                        onChange={this.createNewItem}
+                    />
                     <ul>
                         {items.map(function (item) {
                             return <li>
@@ -32,7 +75,6 @@ let TodoList = React.createClass({
                             </li>
                         }) }
                     </ul>
-
                 </div>
             </div>
         );
